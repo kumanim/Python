3ea848a9a401bf973d4d37e58ee06b812b96543b
1176572 <kumanim@rbos.co.uk>
2023-09-15 06:10:44 +0000

    Day1

---
+++ Newproject/Task3.py
@@ -0,0 +1,12 @@
+'''
+Create a python function to display the square and cube of a given number.
+'''
+n = 4
+
+square = n ** 2
+
+cube = n ** 3
+
+print("\nSquare of the number :", square)
+
+print("Cube of the number :", cube)
